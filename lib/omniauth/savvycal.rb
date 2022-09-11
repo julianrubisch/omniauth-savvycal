1fd84debb07f38b193bd0046f3f1cce93bee3653
Julian Rubisch <julian@julianrubisch.at>
2022-09-11 20:48:07 +0200

    Initial commit

---
+++ lib/omniauth/savvycal.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "savvycal/version"
+
+module Omniauth
+  module Savvycal
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
